2f1f3fbe84577c8ef1e24722d71ba94abdcb9986
Daniel <dalejandrouh.com>
2024-05-04 13:37:28 -0500

    buscar cliente initialize comentado por mientras porque da errores por el momento

--- CommerceApp/src/main/java/com/commerceapp/controller/BuscarClienteController.java
+++ CommerceApp/src/main/java/com/commerceapp/controller/BuscarClienteController.java
@@ -17,75 +17,72 @@ import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 
-public class BuscarClienteController implements Initializable,NavigableControllerHelper{
+public class BuscarClienteController implements Initializable, NavigableControllerHelper {
 	public Control[] controlsInOrderToNavigate;
-    @FXML
-    private Button btnNext;
+	@FXML
+	private Button btnNext;
 
-    @FXML
-    private Button btnPrevio;
+	@FXML
+	private Button btnPrevio;
 
-    @FXML
-    private TableView<?> tblClientes;
+	@FXML
+	private TableView<?> tblClientes;
 
-    @FXML
-    private TextField txtBusquedaCliente;
+	@FXML
+	private TextField txtBusquedaCliente;
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		initializeControlsInOrderToNavigate();
-		registerKeyPressENTERInControlsToNavigate();
-		controlsInOrderToNavigate = new Control[] { idCodigoProducto, idNombreProducto, idDescProducto, idPrecioVenta,
-				idUM, idEanProducto };
-
-		columnId.setCellValueFactory(new PropertyValueFactory<>("tb_product_id"));
-		columnCode.setCellValueFactory(new PropertyValueFactory<>("code"));
-
-		columName.setCellValueFactory(new PropertyValueFactory<>("name"));
-		columnDescription.setCellValueFactory(new PropertyValueFactory<>("description"));
-
-		columnSale.setCellValueFactory(new PropertyValueFactory<>("salesPriceWithTax"));
-		columnUM.setCellValueFactory(new PropertyValueFactory<>("defaultUom"));
-		columnEAN.setCellValueFactory(new PropertyValueFactory<>("ean"));
-
-		cargarProductos();
-		cargarComboUM();
-
-		objJPAControllerProduct = new JPAControllerProduct();
-		Platform.runLater(new Runnable() {
-			@Override
-			public void run() {
-
-				pendienteGuardar = false;
-
-				getParentController().getStagePrincipal().getScene().addEventFilter(KeyEvent.KEY_PRESSED, event -> {
-					if (event.getCode() == KeyCode.TAB) {
-						event.consume(); // Consumir el evento de teclado para evitar la navegación por TAB en todo el
-											// form
-					}
-				});
-
-			}
-		});
-
-		iniciarValidaciones();
-
-		iniciarImageIcon();
-		scroolPane();
-		idBuscarProducto.requestFocus();
-
-		
+		/*
+		 * initializeControlsInOrderToNavigate();
+		 * registerKeyPressENTERInControlsToNavigate(); controlsInOrderToNavigate = new
+		 * Control[] { idCodigoProducto, idNombreProducto, idDescProducto,
+		 * idPrecioVenta, idUM, idEanProducto };
+		 * 
+		 * columnId.setCellValueFactory(new PropertyValueFactory<>("tb_product_id"));
+		 * columnCode.setCellValueFactory(new PropertyValueFactory<>("code"));
+		 * 
+		 * columName.setCellValueFactory(new PropertyValueFactory<>("name"));
+		 * columnDescription.setCellValueFactory(new
+		 * PropertyValueFactory<>("description"));
+		 * 
+		 * columnSale.setCellValueFactory(new
+		 * PropertyValueFactory<>("salesPriceWithTax"));
+		 * columnUM.setCellValueFactory(new PropertyValueFactory<>("defaultUom"));
+		 * columnEAN.setCellValueFactory(new PropertyValueFactory<>("ean"));
+		 * 
+		 * cargarProductos(); cargarComboUM();
+		 * 
+		 * objJPAControllerProduct = new JPAControllerProduct(); Platform.runLater(new
+		 * Runnable() {
+		 * 
+		 * @Override public void run() {
+		 * 
+		 * pendienteGuardar = false;
+		 * 
+		 * getParentController().getStagePrincipal().getScene().addEventFilter(KeyEvent.
+		 * KEY_PRESSED, event -> { if (event.getCode() == KeyCode.TAB) {
+		 * event.consume(); // Consumir el evento de teclado para evitar la navegación
+		 * por TAB en todo el // form } });
+		 * 
+		 * } });
+		 * 
+		 * iniciarValidaciones();
+		 * 
+		 * iniciarImageIcon(); scroolPane(); idBuscarProducto.requestFocus();
+		 * 
+		 */
 	}
-	
+
 	private String busqueda() {
 		return null;
-		
+
 	}
 
 	@Override
 	public void initializeControlsInOrderToNavigate() {
-		controlsInOrderToNavigate = new Control[] {}; // TODO Auto-generated 
-		
+		controlsInOrderToNavigate = new Control[] {}; // TODO Auto-generated
+
 	}
 
 	@Override

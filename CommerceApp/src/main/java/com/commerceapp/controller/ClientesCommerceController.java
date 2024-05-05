c71992ca96bca6f75def2bd6c0ae0350ddef73e5
ByStander-Ox <SergioXD753@hotmail.com>
2024-05-04 21:04:30 -0500

    commit de seguridad

--- CommerceApp/src/main/java/com/commerceapp/controller/ClientesCommerceController.java
+++ CommerceApp/src/main/java/com/commerceapp/controller/ClientesCommerceController.java
@@ -1,5 +1,155 @@
 package com.commerceapp.controller;
 
-public class ClientesCommerceController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import com.commerceapp.controller.helpers.NavigableControllerHelper;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Control;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+public class ClientesCommerceController implements Initializable,NavigableControllerHelper{
+
+    @FXML
+    private GridPane EntidadEmpresarioGB;
+
+    @FXML
+    private GridPane PresentanteGB;
+
+    @FXML
+    private AnchorPane anchorpaneEntradaDatos;
+
+    @FXML
+    private TableColumn<?, ?> columName;
+
+    @FXML
+    private TableColumn<?, ?> columnCode;
+
+    @FXML
+    private TableColumn<?, ?> columnDescuento;
+
+    @FXML
+    private TableColumn<?, ?> columnId;
+
+    @FXML
+    private TableColumn<?, ?> columnNombreComercial;
+
+    @FXML
+    private HBox frmClientes;
+
+    @FXML
+    private Label gbxEntidadEmpresario;
+
+    @FXML
+    private Label gbxPresentante;
+
+    @FXML
+    private Button idActualizarCliente;
+
+    @FXML
+    private TextField idBuscarCliente;
+
+    @FXML
+    private TextField idCodigoCliente;
+
+    @FXML
+    private Button idCrearCliente;
+
+    @FXML
+    private TextField idDescuentoCliente;
+
+    @FXML
+    private Button idDeshacerCreacionCliente;
+
+    @FXML
+    private Button idEliminarCliente;
+
+    @FXML
+    private Button idGuardarCliente;
+
+    @FXML
+    private TextField idNombreCliente;
+
+    @FXML
+    private TextField idNombreComercialCliente;
+
+    @FXML
+    private TableView<?> idTableViewClientes;
+
+    @FXML
+    private VBox idVOBXprueba;
+
+    @FXML
+    private Button idaniadirCliente;
+
+    @FXML
+    private ScrollPane idpruebascroll;
+
+    @FXML
+    private Label lblTitulo;
+
+    @FXML
+    private ImageView logo;
+
+    @FXML
+    void actualizarCliente(ActionEvent event) {
+
+    }
+
+    @FXML
+    void aniadirCliente(ActionEvent event) {
+
+    }
+
+    @FXML
+    void crearCliente(ActionEvent event) {
+
+    }
+
+    @FXML
+    void deshacerCliente(ActionEvent event) {
+
+    }
+
+    @FXML
+    void eliminarCliente(ActionEvent event) {
+
+    }
+
+    @FXML
+    void guardarCliente(ActionEvent event) {
+
+    }
+
+	@Override
+	public void initializeControlsInOrderToNavigate() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Control[] getControlsInOrderToNavigate() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
--- CommerceApp/src/main/java/com/commerceapp/model/Customer.java
+++ CommerceApp/src/main/java/com/commerceapp/model/Customer.java
@@ -33,6 +33,77 @@ public class Customer {
     @Column(name = "updated_at", nullable = false, columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP")
     private LocalDateTime updatedAt;
 
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getFiscalNumber() {
+		return fiscalNumber;
+	}
+
+	public void setFiscalNumber(String fiscalNumber) {
+		this.fiscalNumber = fiscalNumber;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCommercialName() {
+		return commercialName;
+	}
+
+	public void setCommercialName(String commercialName) {
+		this.commercialName = commercialName;
+	}
+
+	public BigDecimal getDiscountProduct() {
+		return discountProduct;
+	}
+
+	public void setDiscountProduct(BigDecimal discountProduct) {
+		this.discountProduct = discountProduct;
+	}
+
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(LocalDateTime createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public LocalDateTime getUpdatedAt() {
+		return updatedAt;
+	}
+
+	public void setUpdatedAt(LocalDateTime updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+
+	@Override
+	public String toString() {
+		return "Customer [id=" + id + ", code=" + code + ", fiscalNumber=" + fiscalNumber + ", name=" + name
+				+ ", commercialName=" + commercialName + ", discountProduct=" + discountProduct + ", createdAt="
+				+ createdAt + ", updatedAt=" + updatedAt + "]";
+	}
+
     // Constructor, getters, and setters
     
 }
